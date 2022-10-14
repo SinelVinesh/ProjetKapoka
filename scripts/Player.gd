f4d82a63a234a786081d45343238128b64f3868b
TsinjoAH <52tsinjo@gmail.com>
2022-10-15 00:21:26 +0300

    player

---
+++ scripts/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+var speed = 200
+
+func _ready():
+	pass # Replace with function body.
+	
+func _process(delta):
+	move(delta)
+
+func move(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	
+
+
