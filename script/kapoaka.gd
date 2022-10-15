794cb38640670192da878adf65601eeb8bd85506
mendrika <randrianarisonmendrikanyaina@gmail.com>
2022-10-15 05:24:37 +0300

    kapoka

--- script/kapoaka.gd
+++ script/kapoaka.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 signal voadaka(val)
 signal entered_cercle(joueur)
+onready var anim=$animation
 
 func _ready():
 	position.x=get_viewport().x/2
@@ -11,11 +12,20 @@ func voadaka(joueur):
 	emit_signal("voadaka")
 	
 func animate_voadaka():
-	
+	$animation.play("tombe")
+	var t = Timer.new()
+	t.set_wait_time(1)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	t.queue_free()
+	$animation.stop()
 	pass
+
 
 
 func _on_cercle_body_entered(body):
 	#print(body. )
-	emit_signal("entered_cercle",body )
+	emit_signal("entered_cercle",body)
 	pass # Replace with function body.
