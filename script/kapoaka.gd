62a73f785d08e0b4a3a83e2be3a500b25d2a2a4e
mendrika <randrianarisonmendrikanyaina@gmail.com>
2022-10-15 13:44:47 +0300

    kapoka

--- script/kapoaka.gd
+++ script/kapoaka.gd
@@ -5,22 +5,14 @@ signal entered_cercle(joueur)
 onready var anim=$animation
 
 func _ready():
-	position.x=get_viewport().x/2
-	position.y=get_viewport().y/2
+	position.x=200
+	position.y=200
 
 func voadaka(joueur):
 	emit_signal("voadaka")
 	
 func animate_voadaka():
-	$animation.play("tombe")
-	var t = Timer.new()
-	t.set_wait_time(1)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	yield(t, "timeout")
-	t.queue_free()
-	$animation.stop()
+	$animation.play("tomber")
 	pass
 
 
@@ -29,3 +21,10 @@ func _on_cercle_body_entered(body):
 	#print(body. )
 	emit_signal("entered_cercle",body)
 	pass # Replace with function body.
+
+
+func _on_Button_pressed():
+	animate_voadaka()
+	pass # Replace with function body.
+
+
